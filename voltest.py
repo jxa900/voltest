4809d1a4f757b900f5c2c74845aa13f6338b033a
woppin@gmail.com <mike@raptop.(none)>
2012-10-30 10:52:28 +1100

    added floating IP to headnode

--- voltest.py
+++ voltest.py
@@ -1,5 +1,9 @@
 from novaclient.v1_1 import client
 from time import sleep
+from fabric.tasks import execute
+
+import os
+import sys
 
 user = os.environ.get('OS_USERNAME')
 tenant = os.environ.get('OS_TENANT_NAME')
@@ -21,7 +25,7 @@ for flavor in cl.flavors.list():
     if flavor.name == 'm1.large':
         large = flavor
 
-num_servers = 8
+num_servers = 0
 
 for i in range(num_servers):
     name = "voltest" + str(i)
@@ -37,9 +41,30 @@ for i in range(num_servers):
 
     current_servers = [s.name for s in cl.servers.list() if s.status == u'ACTIVE']
 
+    # not safe due to ERROR state!
     while name not in current_servers:
         sleep(2)
         print current_servers
         current_servers = [s.name for s in cl.servers.list() if s.status == u'ACTIVE']
 
+# add a public IP to one of the VMs to use as a gateway
+current_servers = [s for s in cl.servers.list() if (s.status == u'ACTIVE' and 'voltest' in s.name)]
+print current_servers
+floating_pool = cl.floating_ip_pools.list()[0]
+floating_ips = cl.floating_ips.list()
+
+free_ips = [ip for ip in cl.floating_ips.list() if ip.instance_id == None]
+print free_ips
+
+# if all our current IPs are being used, try to create a new one?
+# maybe just let it be...
+if not free_ips:
+    print "No free IP addresses to associate with head node"
+    sys.exit()
+     
+head_node_ip = free_ips[0]
+current_servers[0].add_floating_ip(head_node_ip)
+
+# installing pssh and whatnot
+#execute('yum update', hosts=[''])
 
