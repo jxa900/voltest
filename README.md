ce20f33be72f0bfc9c2cb113520097c546ac2162
m <mxc900@mxcnew.(none)>
2012-10-30 15:41:02 +1100

    Logs in to head node and updates. Need to add pssh stuff and installation of things.

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ voltest
 =======
 
 Openstack volume testing script
+
+This test will spin up a cluster of machines on separate physical hosts, and proceed to benchmark the performance of the various storage systems available. The goal is to make it a little easier for ops teams to assess the viability of the many options available when setting up storage for an Openstack cluster.
+
+Storage systems:
+
+VM Local disk
+VM Ephemeral Disk
+volume servive
+
+Tests:
+
+Use iozone/bonnie++
+
+streaming read
+streaming write
+random read
+random write
+
+Fill up the cluster 50%
+Run the same test on each VM and measure performance
+Run a mixture of tests on each VM and measure performance
+
+present data
+???
+profit!
+
--- voltest.py
+++ voltest.py
@@ -1,7 +1,6 @@
 from novaclient.v1_1 import client
 from time import sleep
-from fabric.tasks import execute
-
+from fabric.api import execute, env, local, run
 import os
 import sys
 
@@ -9,6 +8,15 @@ user = os.environ.get('OS_USERNAME')
 tenant = os.environ.get('OS_TENANT_NAME')
 auth_url = os.environ.get('OS_AUTH_URL')
 password = os.environ.get('OS_PASSWORD')
+
+testing_image = 'centos-6-20120921'
+
+# We could make a new one each run and delete at the end
+private_key = 'mxckey'
+
+
+def update():
+    run('yum update')
 
 cl = client.Client(user, password, tenant, auth_url, service_type="compute", insecure=True)
 
@@ -18,14 +26,14 @@ sg = ['open']
 ids  = [s.id for s in servers]
 
 for image in cl.images.list():
-    if image.name == 'centos-6-20120921':
+    if image.name == testing_image:
         basecentos = image
 large = ''
 for flavor in cl.flavors.list():
     if flavor.name == 'm1.large':
         large = flavor
 
-num_servers = 0
+num_servers = 3
 
 for i in range(num_servers):
     name = "voltest" + str(i)
@@ -37,27 +45,23 @@ for i in range(num_servers):
     hints = dict()
     hints['different_host'] = ids
 
-    cl.servers.create(name, image, flavor, key_name='mxckey', scheduler_hints=hints, security_groups=sg)
+    cl.servers.create(name, image, flavor, key_name=private_key, scheduler_hints=hints, security_groups=sg)
 
     current_servers = [s.name for s in cl.servers.list() if s.status == u'ACTIVE']
 
     # not safe due to ERROR state!
     while name not in current_servers:
         sleep(2)
-        print current_servers
+        print "current servers: " + str(current_servers)
         current_servers = [s.name for s in cl.servers.list() if s.status == u'ACTIVE']
 
 # add a public IP to one of the VMs to use as a gateway
 current_servers = [s for s in cl.servers.list() if (s.status == u'ACTIVE' and 'voltest' in s.name)]
-print current_servers
 floating_pool = cl.floating_ip_pools.list()[0]
 floating_ips = cl.floating_ips.list()
 
 free_ips = [ip for ip in cl.floating_ips.list() if ip.instance_id == None]
-print free_ips
 
-# if all our current IPs are being used, try to create a new one?
-# maybe just let it be...
 if not free_ips:
     print "No free IP addresses to associate with head node"
     sys.exit()
@@ -65,6 +69,14 @@ if not free_ips:
 head_node_ip = free_ips[0]
 current_servers[0].add_floating_ip(head_node_ip)
 
-# installing pssh and whatnot
-#execute('yum update', hosts=[''])
+sleep(10)
+
+print current_servers[0]
+
+env.key_filename = private_key + '.private'
+env.user = 'root'
+env.host_string=str(head_node_ip.ip)
+# update headnode and install pssh
+
+execute(update, hosts=[str(head_node_ip.ip)])
 
