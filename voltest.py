2bf5e450543c69cd5fd745fb8f1181065d0c155d
m <mxc900@mxcnew.(none)>
2012-11-01 12:24:24 +1100

    First working version. Runs a 100mb test with different block sizes across the cluster. Currently running on local disk, will add support for ephemeral, volume and maybe FSaaS if there is time

--- voltest.py
+++ voltest.py
@@ -2,6 +2,7 @@ from novaclient.v1_1 import client
 from time import sleep
 from fabric.api import execute, env, local, run
 from os.path import expanduser
+from socket import socket, AF_INET, SOCK_STREAM
 
 import os
 import sys
@@ -13,7 +14,7 @@ tenant = os.environ.get('OS_TENANT_NAME')
 auth_url = os.environ.get('OS_AUTH_URL')
 password = os.environ.get('OS_PASSWORD')
 
-testing_image = 'centos-6-20120921'
+testing_image = 'centos-6-20121101'
 
 # TODO: Could make a new one each run and delete at the end
 private_key = 'mxckey'
@@ -25,16 +26,29 @@ def pssh():
     run('yum install wget -y')
     run('wget http://apt.sw.be/redhat/el6/en/i386/rpmforge/RPMS/pssh-2.3-1.el6.rf.noarch.rpm')
     run('rpm -i pssh-2.3-1.el6.rf.noarch.rpm')
+    run('wget http://pkgs.repoforge.org/iozone/iozone-3.394-1.el6.rf.x86_64.rpm')
 
 def inittest():
-    run('pscp -h .hostlist -l root script /root/script')
-    run('pscp -h .hostlist -l root script /root/initialise')
+    run("pscp -h .hostlist -l root -x '-o StrictHostKeyChecking=no' script /root/script")
+    run("pscp -h .hostlist -l root -x '-o StrictHostKeyChecking=no' initialise /root/initialise")
+    run("pscp -h .hostlist -l root -x '-o StrictHostKeyChecking=no' iozone-3.394-1.el6.rf.x86_64.rpm /root/iozone-3.394-1.el6.rf.x86_64.rpm")
     run("pssh -h .hostlist -l root -t 0 -o /tmp -x '-o StrictHostKeyChecking=no' 'chmod ug+x /root/initialise'")
     run("pssh -h .hostlist -l root -t 0 -o /tmp -x '-o StrictHostKeyChecking=no' ./initialise")
+    run("mkdir -p /root/out")
 
 def runtest():
-    run("pssh -h .hostlist -l root -t 0 -o /tmp -x '-o StrictHostKeyChecking=no' ./script")
+    run("pssh -h .hostlist -l root -t 0 -o /root/out -x '-o StrictHostKeyChecking=no' ./script")
 
+
+def test_port(ip, port, timeout=10):
+    s = socket(AF_INET, SOCK_STREAM)
+    s.settimeout(timeout)
+    result = s.connect_ex((ip, port))
+    s.close()
+    if result == 0:
+        return True
+    else:
+        return False
 
 cl = client.Client(user, password, tenant, auth_url, service_type="compute", insecure=True)
 
@@ -89,13 +103,19 @@ current_servers[0].add_floating_ip(head_node_ip)
 # perhaps open port 22 or something similar
 def check_floating_ip():
     for ip in cl.floating_ips.list():
-        if ip.ip == head_node_ip.ip and ip.instance_id:
+        if ip.ip == head_node_ip.ip and ip.instance_id == current_servers[0].id:
             return True
     return False        
 
 while not check_floating_ip():
     print "waiting for floating ip"
     sleep(1)
+
+# Give the head node 30 seconds to initialise its ssh port after booting
+# before trying to log in
+#if not test_port(head_node_ip.ip, 22, 30):
+#    print "head node failed to respond in a timely manner"
+#    sys.exit()
 
 # get rid of known hosts entry so Fabric doesn't complain
 with open(expanduser('~') + '/.ssh/known_hosts', 'r') as known_hosts:
@@ -106,9 +126,8 @@ with open(expanduser('~') + '/.ssh/known_hosts', 'w') as known_hosts:
         if host.split(' ')[0] != head_node_ip.ip:
             known_hosts.write(host)
 
-sleep(10)
 # push private key out to head node so it can orchestrate
-local('scp -i ' + private_key + '.private' + ' -o StrictHostKeyChecking=no ' + private_key + '.private ' + 'root@' + str(head_node_ip.ip) + ':/root' )
+local('scp -i ' + private_key + '.private' + ' -o StrictHostKeyChecking=no ' + private_key + '.private ' + 'root@' + str(head_node_ip.ip) + ':/root/.ssh/id_rsa' )
 
 # create host list for pssh to use
 with open('.hostlist', 'w+') as hostlist:
@@ -117,6 +136,7 @@ with open('.hostlist', 'w+') as hostlist:
 
 local('scp -i ' + private_key + '.private' + ' -o StrictHostKeyChecking=no ' +  '.hostlist ' + 'root@' + str(head_node_ip.ip) + ':/root' )
 local('scp -i ' + private_key + '.private' + ' -o StrictHostKeyChecking=no ' +  'script ' + 'root@' + str(head_node_ip.ip) + ':/root' )
+local('scp -i ' + private_key + '.private' + ' -o StrictHostKeyChecking=no ' +  'initialise ' + 'root@' + str(head_node_ip.ip) + ':/root' )
 
 # Fabric environment settings
 env.key_filename = private_key + '.private'
@@ -126,7 +146,7 @@ env.host_string=str(head_node_ip.ip)
 # update headnode and install pssh
 execute(update, hosts=[str(head_node_ip.ip)])
 execute(pssh, hosts=[str(head_node_ip.ip)])
-execute(runtest, hosts=[str(head_node_ip.ip)])
 execute(inittest, hosts=[str(head_node_ip.ip)])
+execute(runtest, hosts=[str(head_node_ip.ip)])
 
-local('scp -i ' + private_key + '.private' + '-r -o StrictHostKeyChecking=no ' +  'root@' + str(head_node_ip.ip) + ':/root/out ' + './results' )
+local('scp -i ' + private_key + '.private ' + '-r -o StrictHostKeyChecking=no ' +  'root@' + str(head_node_ip.ip) + ':/root/out ' + './results' )
